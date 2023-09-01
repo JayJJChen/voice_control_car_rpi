7d4f16fcc535332d6307a4bd5f5718b103a67b47
Jiajun Chen <marsyeti@gmail.com>
2023-09-01 22:19:35 +0800

    change wheel GPIO pins to be more organized.

--- modules/car.py
+++ modules/car.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 from modules.config import WHEEL_PINS
 
-# Uncomment the above import when running on a Raspberry Pi
 
 class Wheel:
     def __init__(self, in1, in2):
@@ -9,52 +8,58 @@ class Wheel:
         self.in2 = in2
         GPIO.setup(self.in1, GPIO.OUT)
         GPIO.setup(self.in2, GPIO.OUT)
-    
+
     def forward(self):
         GPIO.output(self.in1, True)
         GPIO.output(self.in2, False)
-    
+
     def backward(self):
         GPIO.output(self.in1, False)
         GPIO.output(self.in2, True)
-    
+
     def stop(self):
         GPIO.output(self.in1, False)
         GPIO.output(self.in2, False)
 
+
 class Car:
     def __init__(self):
-        # GPIO.setmode(GPIO.BOARD)
+
         GPIO.setmode(GPIO.BCM)
-        self.front_left = Wheel(WHEEL_PINS['FRONT_LEFT'][0], WHEEL_PINS['FRONT_LEFT'][1])
-        self.front_right = Wheel(WHEEL_PINS['FRONT_RIGHT'][0], WHEEL_PINS['FRONT_RIGHT'][1])
-        self.back_left = Wheel(WHEEL_PINS['BACK_LEFT'][0], WHEEL_PINS['BACK_LEFT'][1])
-        self.back_right = Wheel(WHEEL_PINS['BACK_RIGHT'][0], WHEEL_PINS['BACK_RIGHT'][1])
-    
+        
+        self.front_left = Wheel(
+            WHEEL_PINS['FRONT_LEFT'][0], WHEEL_PINS['FRONT_LEFT'][1])
+        self.front_right = Wheel(
+            WHEEL_PINS['FRONT_RIGHT'][0], WHEEL_PINS['FRONT_RIGHT'][1])
+        self.back_left = Wheel(
+            WHEEL_PINS['BACK_LEFT'][0], WHEEL_PINS['BACK_LEFT'][1])
+        self.back_right = Wheel(
+            WHEEL_PINS['BACK_RIGHT'][0], WHEEL_PINS['BACK_RIGHT'][1])
+
     def forward(self):
         self.front_left.forward()
         self.front_right.forward()
         self.back_left.forward()
         self.back_right.forward()
-        
+
     def backward(self):
         self.front_left.backward()
         self.front_right.backward()
         self.back_left.backward()
         self.back_right.backward()
-    
+
     def left(self):
         self.front_left.backward()
         self.front_right.forward()
         self.back_left.backward()
         self.back_right.forward()
-    
+
     def right(self):
         self.front_left.forward()
         self.front_right.backward()
         self.back_left.forward()
         self.back_right.backward()
-    
+
     def stop(self):
         self.front_left.stop()
         self.front_right.stop()
--- modules/config.py
+++ modules/config.py
@@ -1,7 +1,7 @@
 # Pin configuration for each wheel (IN1, IN2)
 WHEEL_PINS = {
-    'FRONT_LEFT': (17, 18),
-    'FRONT_RIGHT': (22, 23),
-    'BACK_LEFT': (24, 25),
-    'BACK_RIGHT': (26, 27)
+    'FRONT_LEFT': (4, 14),
+    'FRONT_RIGHT': (17, 18),
+    'BACK_LEFT': (0, 1),
+    'BACK_RIGHT': (6, 12)
 }
